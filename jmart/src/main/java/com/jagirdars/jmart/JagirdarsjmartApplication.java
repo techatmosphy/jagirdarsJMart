31e1f57e154bba6656805cfb471cd38a515c305a
techatmosphy <info@techatmosphy.com>
2022-02-26 02:08:39 +0530

    Initial commit
    Initial commit

---
+++ jmart/src/main/java/com/jagirdars/jmart/JagirdarsjmartApplication.java
@@ -0,0 +1,13 @@
+package com.jagirdars.jmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagirdarsjmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagirdarsjmartApplication.class, args);
+	}
+
+}
